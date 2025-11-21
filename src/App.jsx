ab604f4419e548077da39795eb44cc6d54c9e7e5
Fiona Azumah <fifs@Fionas-MacBook-Pro-2.local>
2025-11-21 01:04:36 -0500

    Refactor App component to remove unused code and integrate Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
-import Navbar from './components/Navbar';
+import Login from './pages/Login.jsx';
+
 
 
 function App() {
   return (
-    <div className="min-h-screen bg-neutral-50 text-neutral-900">
-      <Navbar />
-
-      <div className="p-8">
-        <h1 className="text-3xl font-heading text-gray-700 font-bold">
-          Tailwind is working + Folder structure is set!
-        </h1>
-        <p className="text-primary font-body">
-          Welcome to your WellNest project.
-        </p>
-      </div>
-    </div>
+    <>
+      <Login />
+    </>
   );
 }
+
 
 export default App
