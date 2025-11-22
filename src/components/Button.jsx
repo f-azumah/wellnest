625203c31777aec789e28745fac8f9cf4ec2f581
Fiona Azumah <fifs@Fionas-MacBook-Pro-2.local>
2025-11-22 10:14:05 -0500

    Enhance Button component with font styling and update CSS imports for material symbols

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import "../styles/components.css"
 
-export default function Button({text = " ", className = "bg-lime-200 text-lg text-gray-600 mt-8 flex justify-center items-center" }){
+export default function Button({text = " ", className = "bg-lime-200 text-lg text-gray-600 mt-8 flex justify-center items-center font-sans" }){
 
     return(
         <button className={className}>
