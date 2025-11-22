c9c2fdffc63f046d9a2c04bc5bcf4479964ea9ab
Fiona Azumah <fifs@Fionas-MacBook-Pro-2.local>
2025-11-22 10:12:45 -0500

    Implement routing in App component and enhance Login page with improved layout and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 // import { useState } from 'react'
 import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from './pages/Login.jsx';
+import CreateAccount from './pages/CreateAccount.jsx';
 
 
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/create-account" element={<CreateAccount />} />
+    </Routes>
   );
 }
 
 
-export default App
+export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 // page wrapper/layout
 import Button from "../components/Button";
+import { Link } from "react-router-dom";
 
 function MyComponent(){
     return (
@@ -17,13 +18,13 @@ function Login(){
     return(
         <div className="flex shadow-lg">
         <div className="flex flex-col p-8 rounded-lg w-120 items-center justify-center">
-            <h1 className="text-lg text-black-600 dark:text-white mb-10"> Stay Consistent. Stay Centered. Welcome back to <strong>Wellnest!</strong></h1>
+            <h1 className="text-lg text-black-600 dark:text-white font-serif mb-10"> Stay Consistent. Stay Centered. Welcome back to <strong>Wellnest!</strong></h1>
             <form onSubmit={handleSubmit} className = "space-y-4 flex flex-col items-center">
-                <input type="text" id = "uUsername" name="username" placeholder="username" className="border-solid border-2 border-gray-200 rounded-3xl w-80 h-10"></input>
-                <input type="password" id = "password" name="password" placeholder="password" className="border-solid border-2 border-gray-200 rounded-3xl w-80 h-10"></input>
+                <input type="text" id = "username" name="username" placeholder="Username" className="border-solid border-2 border-gray-200 rounded-3xl w-80 h-10"></input>
+                <input type="password" id = "password" name="password" placeholder="Password" className="border-solid border-2 border-gray-200 rounded-3xl w-80 h-10"></input>
                 
                 <div className="w-80 text-end">
-                <p className="font-semibold text-sm hover:underline-offset-1"><a href="">Forgot password?</a></p>
+                <p className="font-semibold font-serif text-sm hover:underline-offset-1"><a href="">Forgot password?</a></p>
                 </div>
 
                 <Button text="Login"></Button>
@@ -32,7 +33,7 @@ function Login(){
 
             <hr className=" border-gray-300 dark:border-gray-600 my-10 w-80"></hr>
 
-            <p className="mt-4">Not a member? <a href="" className="underline-offset-1 text-lime-700 dark:text-lime-700">Register now</a></p>
+            <p className="mt-4 font-serif">Don't have an account? <Link to="/create-account" className="underline-offset-1 text-lime-700 font-serif dark:text-lime-700">Create one now</Link></p>
         </div>
 
         <div className="p-10">
