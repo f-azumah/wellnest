0c4a875215aa18c50e3c30ba1e1060d9acf23daf
Fiona Azumah <fifs@Fionas-MacBook-Pro-2.local>
2025-11-23 01:09:05 -0500

    Add Dashboard route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from './pages/Login.jsx';
 import CreateAccount from './pages/CreateAccount.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/create-account" element={<CreateAccount />} />
+      <Route path='/dashboard' element={<Dashboard />} />
     </Routes>
   );
 }
