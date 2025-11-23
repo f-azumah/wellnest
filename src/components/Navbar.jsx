a10b3a2c4f85ccbbd6026aa6aca07e6a851fe4ec
Fiona Azumah <fifs@Fionas-MacBook-Pro-2.local>
2025-11-23 01:08:41 -0500

    Refactor Navbar component and add Dashboard page with layout and textarea for user input

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,11 @@
-export default function Navbar() {
-  return (
-    <nav className="w-full bg-blue-600 text-white p-4 flex justify-between items-center">
-      <h1 className="font-bold text-xl">WellNest</h1>
+import { useState } from "react";
 
-      <div className="flex gap-4">
-        <button className="px-3 py-1 bg-white text-blue-600 rounded">
-          Login
-        </button>
-        <button className="px-3 py-1 bg-white text-blue-600 rounded">
-          Sign Up
-        </button>
-      </div>
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+  return (
+    <nav className="fixed top-0 left-0 w-screen bg-white dark:text-gray-800 p-4 flex justify-between items-center shadow-md">
+      <h1 className="font-bold font-heading text-xl">WellNest</h1>
+      <span className="material-symbols-outlined cursor-pointer" onClick={()=>setOpen(!open)}>menu</span>
     </nav>
   );
 }
